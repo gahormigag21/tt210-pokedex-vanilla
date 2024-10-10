09f8a14991da0026c7e7d16d684d3ac02f1eb90a
gahormigag21 <gahormigag@gmail.com>
2024-10-10 18:47:29 -0500

    Añadiendo detalles de pokemons

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const pokemonList = document.getElementById("pokemon-list");
+const pokemonList = document.getElementById("pokemon-list")
+const pokemonDetail = document.getElementById("pokemon-detail")
+const pokemonInfo = document.getElementById("pokemon-info")
+const backButton = document.getElementById("back-button")
 
 async function fetchPokemonData(pokemonId) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
@@ -16,18 +19,40 @@ function displayPokemon(pokemon) {
     pokemonCard.classList.add("pokemon-card");
 
     const sprite = isShiny ? pokemon.sprites.front_shiny : pokemon.sprites.front_default;
-    const types = pokemon.types.map(typeInfo => typeInfo.type.name).join(", ");
+    
     
     pokemonCard.innerHTML = `
         <img src="${sprite}" alt="${pokemon.name}">
         <h3>${pokemon.name.toUpperCase()} ${isShiny ? "(Shiny)" : ""}</h3>
         <p>ID: ${pokemon.id}</p>
-        <p>Type: ${types}</p>
-        <p>Height: ${pokemon.height / 10} m</p>
-        <p>Weight: ${pokemon.weight / 10} kg</p>
     `;
     
+    //linea para mostrar detalles de pokemon
+    pokemonCard.addEventListener("click", ()=>showPokemonDetail(pokemon,isShiny,sprite))    
     pokemonList.appendChild(pokemonCard);
+    return
+}
+
+backButton.addEventListener("click",()=>{
+    pokemonDetail.style.display = "none"
+    pokemonList.style.display = "grid"
+})
+
+function showPokemonDetail(pokemon,isShiny,sprite) {
+    pokemonList.style.display = "none"
+    pokemonDetail.style.display= "block"
+    const types = pokemon.types.map(typeInfo => typeInfo.type.name).join(", ");
+    
+    pokemonInfo.innerHTML = `
+
+    <h2>${pokemon.name.toUpperCase()} ${isShiny ? "(Shiny)" : ""}</h2>
+    <img src="${sprite}" alt="${pokemon.name}">
+    <p>ID: ${pokemon.id}</p>
+    <p>Altura: ${pokemon.height / 10} m</p>
+    <p>Peso: ${pokemon.weight / 10} kg</p>
+    <p>Tipos: ${types}</p>
+    `
+    return
 }
 
 async function loadPokedex() {
